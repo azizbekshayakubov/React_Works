df5f60f96c582f0b34ba17e669cd3fc5eb076ac4
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2025-01-09 18:28:59 +0500

    1:23 minuts

---
+++ 2-React/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from './Header'
+import Footer from './Footer'
+import Main from './Main'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>  
+    </>
+  )
+}
+
+export default App
