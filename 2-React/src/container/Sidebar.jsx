73444bca6328e33687c34490026ae6896d8a4fbd
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2025-01-14 18:38:58 +0500

    39-minuts

--- 2-React/src/container/Sidebar.jsx
+++ 2-React/src/container/Sidebar.jsx
@@ -1,23 +1,33 @@
-import  '../container/sidebar.scss'
+import { Chart, Logo } from '../assets/styles/Icons'
+import '../container/sidebar.scss'
 
 
 const Sidebar = () => {
-return (
-<div className="sidebar">
-    <div className='sidebar-header'>Dashboard Kit</div>
-    <button className='btn sidebar-btn'>Overview</button>
-    <button className='btn sidebar-btn'>Overview</button>
-    <button className='btn sidebar-btn'>Overview</button>
-    <button className='btn sidebar-btn'>Overview</button>
-    <button className='btn sidebar-btn'>Overview</button>
-    <button className='btn sidebar-btn'>Overview</button>
-    <button className='btn sidebar-btn'>Overview</button>
-    <button className='btn sidebar-btn'>Overview</button>
-</div>
+    return (
+        <div className="sidebar">
+            <div className='sidebar-header'>
+                <Logo />
+                <h2>Dashboard Kit</h2>
+            </div>
+            <button className='btn sidebar-btn'>
+                <Chart />
+                <span>
+                    Overview
+                </span>
+            </button>
+            <button className='btn sidebar-btn'>Overview</button>
+            <button className='btn sidebar-btn'>Overview</button>
+            <button className='btn sidebar-btn'>Overview</button>
+            <button className='btn sidebar-btn'>Overview</button>
+            <button className='btn sidebar-btn'>Overview</button>
+            <button className='btn sidebar-btn'>Overview</button>
+            <button className='btn sidebar-btn'>Overview</button>
+        </div>
 
 
 
-)}
+    )
+}
 
 
 export default Sidebar
